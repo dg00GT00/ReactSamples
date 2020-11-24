5c7939a9f149493a6d38fbc305a5568a0f091ebe
Gledson <duarte.gledson@gmail.com>
2020-11-24 08:04:40 -0300

    Using links to switch pages

--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -1,18 +1,35 @@
 import * as React from "react";
+import { Route, Link } from "react-router-dom";
+import Posts from "../Posts/Posts";
+import NewPost from "../NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends React.Component {
 	render() {
 		return (
 			<div>
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-posts">New Post</a></li>
-                        </ul>
-                    </nav>
-                </header>
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<Link to={"/"}>Home</Link>
+							</li>
+							<li>
+								<Link
+									to={{
+										pathname: "/new-post",
+										hash: "#submit",
+										search: "?quick-submit=true",
+									}}
+								>
+									New Post
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<Route path={"/"} exact component={Posts} />
+				<Route path={"/new-post"} component={NewPost} />
 			</div>
 		);
 	}
