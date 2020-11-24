056dbfd6086456bf1a68f4b8ebd829855c934825
Gledson <duarte.gledson@gmail.com>
2020-11-24 20:11:59 -0300

    Getting started with useState()

--- src/components/Ingredients/IngredientForm.tsx
+++ src/components/Ingredients/IngredientForm.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import "./IngredientForm.css";
 
+type IngredientState = {
+	title?: string;
+	amount?: string;
+};
+
 const IngredientForm = React.memo((props) => {
+	const [inputState, changeState] = useState<IngredientState>({
+		title: "",
+		amount: "",
+	});
+
 	const submitHandler: React.FormEventHandler = (event) => {
 		event.preventDefault();
 		// ...
@@ -15,11 +25,31 @@ const IngredientForm = React.memo((props) => {
 				<form onSubmit={submitHandler}>
 					<div className="form-control">
 						<label htmlFor="title">Name</label>
-						<input type="text" id="title" />
+						<input
+							type="text"
+							id="title"
+							value={inputState.title}
+							onChange={(event) =>
+								changeState((previousInputState) => ({
+									...previousInputState,
+									title: event.target.value,
+								}))
+							}
+						/>
 					</div>
 					<div className="form-control">
 						<label htmlFor="amount">Amount</label>
-						<input type="number" id="amount" />
+						<input
+							type="number"
+							id="amount"
+							value={inputState.amount}
+							onChange={(event) =>
+								changeState((previousInputState) => ({
+									...previousInputState,
+									amount: event.target.value,
+								}))
+							}
+						/>
 					</div>
 					<div className="ingredient-form__actions">
 						<button type="submit">Add Ingredient</button>
